43fc97f5aa23d7e7e8cf0aa465431e5d7a500779
Sviatoslav Abramov <jokhiop19@gmail.com>
2021-04-25 12:43:18 +0300

    Navbar added

--- lalaloo/settings/base.py
+++ lalaloo/settings/base.py
@@ -1,9 +1,6 @@
 import os, sys
 
-from dotenv import load_dotenv
 from pathlib import Path
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
