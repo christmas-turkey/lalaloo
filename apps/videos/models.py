b776c9c70466a88ef248d67d0b9839ebb1b060ed
Sviatoslav Abramov <jokhiop19@gmail.com>
2021-04-26 21:56:34 +0300

    Video preview cards added

--- apps/videos/models.py
+++ apps/videos/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Video(models.Model):
   title = models.CharField(max_length=128)
@@ -6,4 +9,11 @@ class Video(models.Model):
   date = models.DateTimeField(auto_now_add=True)
   topics = models.CharField(max_length=128)
   likes = models.IntegerField(default=0)
+  dislikes = models.IntegerField(default=0)
+  views = models.IntegerField(default=0)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
   video = models.FileField(upload_to="%Y/%m/%d")
+
+  def is_recently_uploaded(self):
+    return self.date >= (timezone.now() - timezone.timedelta(days=7))
+
